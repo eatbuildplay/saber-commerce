a849e3291c79e435fdc171b65aab7361e0401728
Casey Milne <casey.milne@eatbuildplay.com>
2021-02-27 21:25:22 -0500

    draft main plugin file

---
+++ saber-commerce.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * Plugin Name: Saber Commerce
+ * Plugin URI: https://eatbuildplay.com/plugins/saber-commerce/
+ * Description: Powerful ecommerce software for WordPress sites.
+ * Version: 1.0.0
+ * Author: Eat/Build/Play
+ * Author URI: https://eatbuildplay.com/
+ * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
+
+class Plugin {
+
+	public function __construct() {
+
+	}
+
+	public static function activation() {
+
+	}
+
+	public static function deactivation() {
+
+	}
+
+}
+
+new Plugin();
