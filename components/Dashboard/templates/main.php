491e94afec714e57e4590f30c4317ec00d5cf342
Casey Milne <casey.milne@eatbuildplay.com>
2021-03-01 09:36:27 -0500

    dashboard layout

--- components/Dashboard/templates/main.php
+++ components/Dashboard/templates/main.php
@@ -4,22 +4,27 @@
 
 ?>
 
-
-<h1>DASHBOARD</h1>
-
-<ul id="sacom-dashboard-menu">
-	<li data-section="payments">PAYMENTS</li>
-	<li data-section="invoices">INVOICES</li>
-	<li data-section="timesheets">TIMESHEETS</li>
-	<li data-section="profile">PROFILE</li>
-	<li data-section="users">USERS</li>
-	<li data-section="logout">LOGOUT</li>
-</ul>
-
 <div class="container">
 	<div class="row">
+
+		<div class="col-4">
+
+			<h1>DASHBOARD</h1>
+
+			<ul id="sacom-dashboard-menu">
+				<li data-section="payments">PAYMENTS</li>
+				<li data-section="invoices">INVOICES</li>
+				<li data-section="timesheets">TIMESHEETS</li>
+				<li data-section="profile">PROFILE</li>
+				<li data-section="users">USERS</li>
+				<li data-section="logout">LOGOUT</li>
+			</ul>
+
+		</div>
+
 		<div class="col-md-8">
 			<div id="dashboard-canvas"></div>
 		</div><!-- ./col -->
+
 	</div><!-- ./row -->
 </div><!-- ./container -->
